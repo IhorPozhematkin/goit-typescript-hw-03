13dcdb58b94f35dcb4ab18b09c8ad22cc9ca488c
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-12-28 16:05:56 +0200

    goit-typescript-hw-03 28.12-1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+interface IKey {
+  unlock(): void;
+}
+
+class Key implements IKey {
+  unlock(): void {
+    console.log("The door is unlocked!");
+  }
+}
+
+class Person {
+  private key: IKey;
+
+  constructor(key: IKey) {
+    this.key = key;
+  }
+
+  getKey(): IKey {
+    return this.key;
+  }
+}
+
+class MyHouse {
+  private key: IKey;
+
+  constructor(key: IKey) {
+    this.key = key;
+  }
+
+  openDoor(key: IKey): void {
+    if (key === this.key) {
+      key.unlock();
+      console.log("The door is opened.");
+    } else {
+      console.log("Invalid key");
+    }
+  }
+
+  comeIn(person: Person): void {
+    console.log("Welcome!)");
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
